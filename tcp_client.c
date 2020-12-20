f9aaf8d2767c1329e4e21c4884092dad381967e7
Jan.Holubcik <jan.holubcik1@gmail.com>
2020-12-20 11:12:17 +0100

    pridanie tcp clienta

--- tcp_client.c
+++ tcp_client.c
@@ -1,4 +1,81 @@
 //
 // Created by janho on 20.12.2020.
 //
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
+int main(int argc, char *argv[])
+{
+    int sockfd, n;
+    struct sockaddr_in serv_addr;
+    struct hostent* server;
+
+    char buffer[256];
+
+    if (argc < 3)
+    {
+        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+        return 1;
+    }
+
+    server = gethostbyname(argv[1]); //Použijeme funkciu gethostbyname na získanie informácii o počítači, ktorého hostname je v prvom argumente.
+    if (server == NULL)
+    {
+        fprintf(stderr, "Error, no such host\n");
+        return 2;
+    }
+
+    bzero((char*)&serv_addr, sizeof(serv_addr));
+//Vynulujeme a zinicializujeme adresu, na ktorú sa budeme pripájať.
+    serv_addr.sin_family = AF_INET;
+    bcopy(
+            (char*)server->h_addr,
+            (char*)&serv_addr.sin_addr.s_addr,
+            server->h_length
+    );
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    //Vytvoríme si socket v doméne
+    if (sockfd < 0)
+    {
+        perror("Error creating socket");
+        return 3;
+    }
+ //Pripojíme sa na zadanú sieťovú adresu
+    if(connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("Error connecting to socket");
+        return 4;
+    }
+
+    printf("Please enter a message: "); //Vyzveme používateľa aby zadal text správy pre server
+    bzero(buffer,256);
+    fgets(buffer, 255, stdin);//Načítame správu od používateľa zo štandardného vstupu do buffra.
+
+    n = write(sockfd, buffer, strlen(buffer));//Pošleme správu cez socket servru.
+    if (n < 0)
+    {
+        perror("Error writing to socket");
+        return 5;
+    }
+
+    bzero(buffer,256);
+    n = read(sockfd, buffer, 255);//Načítame odpoveď od servra do buffra.
+    if (n < 0)
+    {
+        perror("Error reading from socket");
+        return 6;
+    }
+
+    printf("%s\n",buffer);//Vypíšeme odpoveď z buffra na štandardný výstup.
+    close(sockfd);
+
+    return 0;
+}
