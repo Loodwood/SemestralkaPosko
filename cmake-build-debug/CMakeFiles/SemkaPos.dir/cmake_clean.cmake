8d6a82612675b7cee1090b05c78b2882f1507e2e
Martin Gajdos <martingajdos.zs@gmail.com>
2020-12-20 09:52:15 +0100

    first comit

---
+++ cmake-build-debug/CMakeFiles/SemkaPos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SemkaPos.dir/main.c.o"
+  "SemkaPos.pdb"
+  "SemkaPos"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SemkaPos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
