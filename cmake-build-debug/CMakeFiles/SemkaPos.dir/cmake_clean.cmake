1770e27def785ebef1791ba0d1c90cc0756f041a
Martin Gajdos <martingajdos.zs@gmail.com>
2020-12-20 11:41:36 +0100

    edit c.file ZHTPP

--- cmake-build-debug/CMakeFiles/SemkaPos.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/SemkaPos.dir/cmake_clean.cmake
@@ -1,5 +1,5 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/SemkaPos.dir/server.c.o"
+  "CMakeFiles/SemkaPos.dir/HTTP_server.c.o"
   "SemkaPos.pdb"
   "SemkaPos"
 )
--- tcp_client.c
+++ tcp_client.c
@@ -12,6 +12,7 @@
 
 int main(int argc, char *argv[])
 {
+    //
     int sockfd, n;
     struct sockaddr_in serv_addr;
     struct hostent* server;
