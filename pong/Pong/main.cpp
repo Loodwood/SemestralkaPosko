23eb61ff4461d15ac5e111c1b0dd0932c9a6fb2f
Martin Gajdos <martingajdos.zs@gmail.com>
2020-12-29 09:16:38 +0100

    create net

--- pong/Pong/main.cpp
+++ pong/Pong/main.cpp
@@ -20,15 +20,17 @@ using namespace std;
 /*
  * 
  */
-int main(int argc, char** argv) {
-    
- const int WINDOW_WIDTH = 1280;
+// konstasnty pre nase hracie okno 
+const int WINDOW_WIDTH = 1280;
 const int WINDOW_HEIGHT = 720;
 
-
+int main(int argc, char** argv) {
+    
+    
 	// Initialize SDL components
 	SDL_Init(SDL_INIT_VIDEO);
 
+        // vytvorit okno kde chceme vykreslovat hru 
 	SDL_Window* window = SDL_CreateWindow("Pong", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
@@ -46,6 +48,7 @@ const int WINDOW_HEIGHT = 720;
 				{
 					running = false;
 				}
+                                // pozriem sa ci som stlacil esc 
 				else if (event.type == SDL_KEYDOWN)
 				{
 					if (event.key.keysym.sym == SDLK_ESCAPE)
@@ -58,10 +61,20 @@ const int WINDOW_HEIGHT = 720;
 			// Clear the window to black
 			SDL_SetRenderDrawColor(renderer, 0x0, 0x0, 0x0, 0xFF);
 			SDL_RenderClear(renderer);
+                        
+                        // Set the draw color to be white
+                       SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
-			//
-			// Rendering will happen here
-			//
+			// Draw the net
+                       for(int y = 0; y < WINDOW_HEIGHT; ++y) {
+                           
+                           // aby sme vytvorili prerusovanu ciaru  kazdy
+                           if(y % 5) {
+                               
+                               SDL_RenderDrawPoint(renderer,WINDOW_WIDTH/2,y);
+                           }
+                       
+                       }
 
 			// Present the backbuffer
 			SDL_RenderPresent(renderer);
